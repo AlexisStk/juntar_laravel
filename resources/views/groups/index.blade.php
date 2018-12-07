f63359f6e96373a684ac9a6a9ec4f921f159d630
andydav484 <adavalos484@gmail.com>
2018-12-07 00:18:28 -0300

    algo

--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -5,50 +5,7 @@
     <title>Grupos!</title>
 </head>
 <body class="bodyAzul">
-    {{-- @include('layouts.navbar') --}}
-    <nav class="navbar fixed-top navbar-expand-md navbar-dark bg-dark">
-        <div class="container">
-            <button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-    
-            <div class="collapse navbar-collapse" id="collapse_target">
-    
-                <a class="navbar-brand text-center"href="/"><img src="{{ asset ('svg/logo.png') }}"></a>
-    
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/inicio">Inicio</a>
-                    </li>
-                    
-                    <li class="nav-item">
-                        <a class="nav-link" href="/grupos">Mas Grupos!</a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link" href="/perfil">Mi Perfil</a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('logout') }}"
-                           onclick="event.preventDefault();
-                                         document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                        </a>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </li>
-
-            </ul>
-        </div>
-    </div>
-
-</nav>
-<br>
-        <br>
-        <br>
-        <br>    
+    @include('layouts.navbar')
         <br>
 <div class="card" style="width: 18rem;">
         
